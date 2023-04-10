496817f911824d946e7f0c0656d6b01bcb1ff7b8
Hariprasanth-HP <harireactjs@outlook.com>
2023-04-10 13:10:39 +0530

    latest

--- src/store/Display/action.js
+++ src/store/Display/action.js
@@ -1,20 +1,3 @@
-export const addEmployee = (emp, isUpdate) => ({
-  type: "ADD_EMPLOYEE",
-  payload1: emp,
-  payload2: isUpdate,
-});
-export const editEmployee = (index, isedit) => ({
-  type: "EDIT_EMPLOYEE",
-  payload1: index,
-  payload2: isedit,
-});
-export const updateEmployee = (emp, index, isupdate) => ({
-  type: "UPDATE_EMPLOYEE",
-  payload1: emp,
-  payload2: index,
-  payload3: isupdate,
-});
-export const deleteEmployee = (index) => ({
-  type: "DELETE_EMPLOYEE",
-  payload: index,
-});
+export const username = (user) => ({ type: "USER_NAME", payload: user });
+export const reset = (user) => ({ type: "RESET", payload: user });
+export const value = (val) => ({ type: "VALUES", payload: val });
--- src/store/Display/constants.js
+++ src/store/Display/constants.js
@@ -1,4 +1,3 @@
-export const ADD_EMPLOYEE = "ADD_EMPLOYEE";
-export const UPDATE_EMPLOYEE = "UPDATE_EMPLOYEE";
-export const EDIT_EMPLOYEE = "EDIT_EMPLOYEE";
-export const DELETE_EMPLOYEE = "DELETE_EMPLOYEE";
+export const USER_NAME = "USER_NAME";
+export const RESET = "RESET";
+export const VALUES = "VALUES";
